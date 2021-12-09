afbb998a73b27afd799c0e6160d9fd469eefca83
James Keicher <jamesdkeicher@gmail.com>
2021-12-09 09:02:47 -0800

    Needs refactor to meet rubric. Will resubmit on Friday

--- app.js
+++ app.js
@@ -40,32 +40,59 @@ const createPoll = (data) => {
 
   const btn1 = document.createElement('button')
   btn1.setAttribute('id', 'btn1')
-  btn1.textContent = option1
+  btn1.textContent = option1 + ' +'
+
+  const btn1a = document.createElement('button')
+  btn1a.setAttribute('id', 'btn1a')
+  btn1a.textContent = option1 + ' -'
 
   const btn2 = document.createElement('button')
   btn2.setAttribute('id', 'btn2')
-  btn2.textContent = option2
+  btn2.textContent = option2 + ' +'
+
+  const btn2a = document.createElement('button')
+  btn2a.setAttribute('id', 'btn2a')
+  btn2a.textContent = option2 + ' -'
 
   const btn3 = document.createElement('button')
   btn3.setAttribute('id', 'btn3')
-  btn3.textContent = option3
+  btn3.textContent = option3 + ' +'
+
+  const btn3a = document.createElement('button')
+  btn3.setAttribute('id', 'btn3a')
+  btn3a.textContent = option3 + ' -'
 
   const closePoll = document.createElement('button')
   closePoll.textContent = "Close Poll"
 
-  optDiv.append(btn1, btn2, btn3)
+  optDiv.append(btn1, btn1a, btn2, btn2a, btn3, btn3a)
   pollContainer.append(questionDisplay, optDiv, closePoll)
 
   btn1.addEventListener('click', () => {
     firstOption++;
     console.log(firstOption);
   })
+  btn1a.addEventListener('click', () => {
+    firstOption--;
+    console.log(firstOption);
+  })
   btn2.addEventListener('click', () => {
     secondOption++;
     console.log(secondOption);
   })
+
+  btn2a.addEventListener('click', () => {
+    secondOption--;
+    console.log(secondOption);
+  })
+
   btn3.addEventListener('click', () => {
     thirdOption++;
+    console.log(thirdOption);
+  })
+
+  btn3a.addEventListener('click', () => {
+    thirdOption--;
     console.log(thirdOption);
   })
 
@@ -82,7 +109,10 @@ const renderClosedPolls = (question, option1, option2, option3) => {
     question,
     option1,
     option2,
-    option3
+    option3,
+    firstOption,
+    secondOption,
+    thirdOption
   }
   closedPollsArray.push(pollObject)
 
@@ -93,11 +123,11 @@ const renderClosedPolls = (question, option1, option2, option3) => {
     const closedQuestion = document.createElement('h3')
     closedQuestion.textContent = poll.question
     const closedOpt1 = document.createElement('p')
-    closedOpt1.textContent = `${poll.option1} Votes: ${firstOption}`
+    closedOpt1.textContent = `${poll.option1} Votes: ${poll.firstOption}`
     const closedOpt2 = document.createElement('p')
-    closedOpt2.textContent = `${poll.option2} Votes: ${secondOption}`
+    closedOpt2.textContent = `${poll.option2} Votes: ${poll.secondOption}`
     const closedOpt3 = document.createElement('p')
-    closedOpt3.textContent = `${poll.option3} Votes: ${thirdOption}`
+    closedOpt3.textContent = `${poll.option3} Votes: ${poll.thirdOption}`
   
     closedDiv.append(closedQuestion, closedOpt1, closedOpt2, closedOpt3)
   }
